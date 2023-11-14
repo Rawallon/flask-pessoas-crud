bf39a1664f43d2d2d2e10574b6cbf63f21630ff7
Rawallon Cardoso <rawallon@gmail.com>
2023-11-14 14:11:14 -0300

    WIP: Template implementation

--- app/main.py
+++ app/main.py
@@ -1,15 +1,17 @@
 from fastapi import FastAPI
+from fastapi.templating import Jinja2Templates
 
 from app import fill_db, models
 from app.database import engine
+from app.routers.frontend import router as frontRouter
 from app.routers.pessoas import pessoasRouter
 
 models.Base.metadata.create_all(bind=engine)
 
-app = FastAPI()
-
-fill_db.fill_db()
 app = FastAPI(
     title="Pessoas Conhecidas Inc.", description="Lista todas as pessoas conhecidas"
 )
-app.include_router(pessoasRouter, prefix="", tags=["pessoa"])
+
+fill_db.fill_db()
+app.include_router(pessoasRouter, prefix="/api")
+app.include_router(frontRouter, prefix="")
